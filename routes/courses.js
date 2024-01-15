c3599bdd205a1c6b35ff2fa62d55d684fa4d4c09
aousjosef <aoussabah@gmail.com>
2024-01-15 04:17:44 +0100

    added delete req

--- routes/courses.js
+++ routes/courses.js
@@ -1,10 +1,10 @@
-import express from "express";
+import express, { json } from "express";
 import fs from "fs/promises";
 
 const router = express.Router();
 const filePath = "./courses.json";
 
-let jsonData;
+let jsonData = [];
 
 // Read file
 async function readFile() {
@@ -27,35 +27,61 @@ async function writeFile(data) {
   }
 }
 
-// Add new course to data
-function addNewCourse(id, courseId, courseName, coursePeriod) {
-  if (!id || !courseId || !courseName || !coursePeriod) {
-    return console.log("ERROR: Something is empty");
-  }
+// * * ** * * *ROUTES * * * * *
 
-  const newObj = {
-    _id: id,
-    courseId: courseId,
-    courseName: courseName,
-    coursePeriod: coursePeriod,
-  };
-
-  jsonData.push(newObj);
-}
-
-// All routes start from /courses route
+// GET ALL ROUTE
 router.get("/", async (req, res) => {
   await readFile();
   res.send(jsonData);
 });
 
+// POST ROUTE
 router.post("/", async (req, res) => {
   await readFile();
-  addNewCourse(225, "DT543", "Aous 22", 23);
+  const userInput = req.body;
+  jsonData.push(userInput);
   await writeFile(jsonData);
+  console.log(typeof jsonData);
   res.send(jsonData);
 });
 
-console.log("hello from routerCourse");
+//GET BY ID ROUTE
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  await readFile();
+  const getCourseById = jsonData.find((course) => {
+    return course._id == id;
+  });
+  if (!getCourseById) {
+    res.status(404).send("Course not found");
+  } else {
+    res.send(getCourseById);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const idToDelete = parseInt(req.params.id);
+  await readFile();
+
+  // Find the index of the course with the specified _id
+  const indexToDelete = jsonData.findIndex(
+    (course) => course._id === idToDelete
+  );
+
+  if (indexToDelete === -1) {
+    res.status(404).send("Course not found");
+  } else {
+    // Remove the course from the array
+    jsonData.splice(indexToDelete, 1);
+
+    // Write the updated data back to the file
+    await writeFile(jsonData);
+
+    res.send({
+      message: "Course deleted successfully",
+      deletedCourseId: idToDelete,
+    });
+  }
+});
 
 export default router;
