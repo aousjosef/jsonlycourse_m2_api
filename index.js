1284454d487038006a69c246a088cfde92b245c5
aousjosef <aoussabah@gmail.com>
2024-01-16 00:47:18 +0100

    added cors

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 import courseRoutes from "./routes/courses.js";
+import cors from "cors";
 
 const app = express();
 
 const PORT = 5000;
 
 app.use(bodyParser.json());
-
+app.use(cors());
 app.use('/courses', courseRoutes);
 
 app.get("/", (req, res) => {
